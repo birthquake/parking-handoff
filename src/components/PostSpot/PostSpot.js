f01bb5d10e3eb8ee7ba751418d3a791121cf696f
birthquake <rashiedtyre@gmail.com>
2025-09-21 21:15:56 -0400

    Update PostSpot.js

--- src/components/PostSpot/PostSpot.js
+++ src/components/PostSpot/PostSpot.js
@@ -267,7 +267,6 @@ const PostSpot = ({ user }) => {
                     required
                     value={formData.availableAt}
                     onChange={handleInputChange}
-                    min={getMinTime()}
                     className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500"
                   />
                 </div>

1f39caa6c75c90ed4219568271aeae329afac6cc
birthquake <rashiedtyre@gmail.com>
2025-09-21 21:57:58 -0400

    Update Navigation.js

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -67,6 +67,17 @@ const Navigation = ({ user }) => {
               </Link>
               
               <Link
+                to="/my-spots"
+                className={`${
+                  isActive('/my-spots')
+                    ? 'border-blue-500 text-gray-900'
+                    : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300'
+                } inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium`}
+              >
+                My Spots
+              </Link>
+              
+              <Link
                 to="/messages"
                 className={`${
                   isActive('/messages')
